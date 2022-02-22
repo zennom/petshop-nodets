529c5b0b6240cbc7d50c75dcd7bbd46253654441
Viviane de Lima <delimaviviane@yahoo.com.br>
2022-02-22 19:48:58 -0300

    rotas criadas

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,15 +1,34 @@
 import {Request, Response} from 'express'
 
-
 export const home = (req:Request, res: Response) =>{
-    res.send("Teste")
+    res.render("pages/pages",{
+        banner:{
+            title:'Todos os animais',
+            background:'allanimals.jpg'
+        }
+    })
 }
-
 export const dogs = (req:Request, res: Response) =>{
+    res.render("pages/pages",{
+        banner:{
+            title:'Todos os cachorros',
+            background:'banner_dog.jpg'
+        }
+    })
 }
-
 export const cats = (req:Request, res: Response) =>{
+    res.render("pages/pages",{
+        banner:{
+            title:'Todos os gatos',
+            background:'banner_cat.jpg'
+        }
+    })
 }
-
 export const fishes = (req:Request, res: Response) =>{
+    res.render("pages/pages",{
+        banner:{
+            title:'Todos os peixes',
+            background:'banner_fish.jpg'
+        }
+    })
 }
